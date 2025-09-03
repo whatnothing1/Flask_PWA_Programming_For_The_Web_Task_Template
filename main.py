d79d122f6cf5e15fba9a6b9dd2d3db5c56bb76de
Vivaan Varma <vivaan.varma@education.nsw.gov.au>
2025-09-03 15:03:12 +1000

    did my first home page

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import database_manager as dbHandler
+
+app = Flask(__name__)
+
+@app.route('/index.html', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
+def index():
+   data = dbHandler.listExtension()
+   return render_template('/index.html', content=data)
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=5100)
