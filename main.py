c5a5659e6f19e7cbd0bc1caba5a2f717f5a8afe9
Vivaan Varma <vivaan.varma@education.nsw.gov.au>
2025-09-17 14:39:41 +1000

    make chnages to styling all my 3 pages

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 import database_manager as dbHandler
 
 app = Flask(__name__)
@@ -8,9 +6,16 @@ app = Flask(__name__)
 @app.route('/index.html', methods=['GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
-   data = dbHandler.listExtension()
-   return render_template('/index.html', content=data)
+    data = dbHandler.listExtension()
+    return render_template('index.html', content=data)
 
+@app.route('/games', methods=['GET'])
+def games():
+    return render_template('games.html')
+
+@app.route('/players', methods=['GET'])
+def show_players():
+    return render_template('partials/players.html')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5100)
+    app.run(debug=True, host='0.0.0.0', port=5100)
